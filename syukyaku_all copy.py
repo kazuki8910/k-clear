501ec2698ecbd06e974e3e6b9dd97f1be97edc5b
*q91fTG8Von <kumagikazu@gmail.com>
2022-07-21 12:08:33 +0900

    基幹データをアップできるように

--- syukyaku_all copy.py
+++ syukyaku_all copy.py
@@ -5,19 +5,6 @@
 
 
 # 集客表のデータ取得
-# クランROASシートの基幹データ更新
-
-
-########################
-# 
-# 設定（変動）
-# 
-########################
-
-# 該当月
-applicable_month = 7
-
-
 
 
 ########################
@@ -31,11 +18,7 @@ import pandas as pd
 import re
 import pytz
 import sys
-import os
-import csv
-import time
 from datetime import datetime as dt
-from selenium.webdriver.common.by import By
 
 # スプシ関連
 from gspread_dataframe import set_with_dataframe
@@ -89,97 +72,6 @@ def update_sheet(key_list, name_to, df_origin):
 
             except:
                 print(asp_name + "更新失敗")
-
-
-
-
-########################
-# 
-# クランの基幹タブ更新
-# 
-########################
-########################
-# 変数
-########################
-# 基幹システムのURL
-url_kikan = "https://mensclear.com/users/reservations"
-# ログイン情報
-login_email = "cb140000@yahoo.co.jp" # メールアドレス
-login_pass  = "muramura" # パスワード
-# CSVファイルの保存先
-csv_filepass = "data/reservations.csv"
-# 実行月
-this_month = dt.today().month
-
-#########################
-# 月をまたいだら実行しない
-#########################
-if(this_month == applicable_month):
-
-    # クローム軌道
-    drive = func.start_chrome()
-
-    #########################
-    # 基幹システムにログイン
-    #########################
-
-    drive.get(url_kikan)
-    time.sleep(3)
-
-    # メールに入力
-    elm_email = drive.find_element(By.ID, "user_email")
-    elm_email.send_keys(login_email)
-    time.sleep(1)
-
-    # パスワードを入力
-    elm_pass = drive.find_element(By.ID, "user_password")
-    elm_pass.send_keys(login_pass)
-    time.sleep(1)
-
-    # ログインボタン押下
-    elm_login_btn = drive.find_element(By.CLASS_NAME, "p-sessions__button")
-    elm_login_btn.click()
-    time.sleep(3)
-
-    #########################
-    # CSVダウンロード
-    #########################
-
-    # データ一覧を表示
-    elm_data_index = drive.find_elements(By.CLASS_NAME, "l-dashboard__panel")[0]
-    elm_data_index.click()
-    time.sleep(3)
-
-    # ダウンロードボタンをクリック
-    elm_dl_btn = drive.find_element(By.NAME, "commit")
-    elm_dl_btn.click()
-    time.sleep(3)
-
-    #########################
-    # スプシにアップロード
-    #########################
-
-    # クラン ROAS管理シートキー
-    sheet_key = "18s2UpmJ5z0EyMxepWVOZ7gDnQ8y5SOBF9Sma3P8Enws"
-
-    # シートにアクセス
-    wb = func.connect_gspread(sheet_key)
-
-    # CSVをアップロード
-    wb.values_update(
-        '基幹_当月',
-        params={'valueInputOption': 'USER_ENTERED'},
-        body={'values': list(csv.reader(open(csv_filepass, encoding='cp932')))}
-    )
-    time.sleep(3)
-
-    #########################
-    # CSVファイルを削除
-    #########################
-
-    os.remove(csv_filepass)
-
-
 
 
 
